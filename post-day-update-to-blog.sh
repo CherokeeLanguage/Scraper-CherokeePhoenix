1f184162456c0e7ecd8a4e95a47d51ab45920fc8
ᎹᎦᎵ ᏦᎢᏁ <mjoyner@vbservices.net>
2018-09-15 11:46:13 -0400

    Add scripts to use java steem cli poster to post updates to steem blog. Create artwork for this purpose.
    	new file:   artwork/Scraper.svg
    	new file:   artwork/logo.svg
    	new file:   "artwork/\341\217\243\341\216\263\341\216\251-\341\217\247\341\216\264\341\216\257\341\217\214\341\217\205\341\216\257-\341\216\244\341\217\215\341\217\233\341\216\252\341\217\215\341\217\231\341\217\227.png"
    	new file:   "artwork/\341\217\243\341\216\263\341\216\251-\341\217\247\341\216\264\341\216\257\341\217\214\341\217\205\341\216\257-\341\216\244\341\217\215\341\217\233\341\216\252\341\217\215\341\217\231\341\217\227.svg"
    	new file:   post-day-update-to-blog.sh
    	new file:   post-week-update-to-blog.sh

---
+++ post-day-update-to-blog.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+cd "$(dirname "$0")"
+
+IMG="![ᏣᎳᎩ-ᏧᎴᎯᏌᏅᎯ-ᎤᏍᏛᎪᏍᏙᏗ.png](https://cdn.steemitimages.com/DQmbrTUET1HhhfPDDVSqaDzXB6m9Xicrq1qRBYB2DHWb6Pw/%E1%8F%A3%E1%8E%B3%E1%8E%A9-%E1%8F%A7%E1%8E%B4%E1%8E%AF%E1%8F%8C%E1%8F%85%E1%8E%AF-%E1%8E%A4%E1%8F%8D%E1%8F%9B%E1%8E%AA%E1%8F%8D%E1%8F%99%E1%8F%97.png)"
+
+when=$(git log -1 --pretty=format:'%ci')
+
+mkdir tmp 2> /dev/null || true
+touch tmp/log.old
+git log --branches=\* --after="1 week ago" > tmp/log.new
+diff tmp/log.new tmp/log.old > /dev/null 2>&1 && exit
+if [ ! -s tmp/log.new ]; then exit; fi
+
+echo "Posting new message"
+msgfile="tmp/msg.txt"
+cp /dev/null "$msgfile"
+echo "title: Updates for $(basename $(pwd)) within the past day. [$when]"  >> "$msgfile"
+echo "tags: utopian-io steemdev java gwt steem-js"  >> "$msgfile"
+echo "format: markdown"  >> "$msgfile"
+echo "" >> "$msgfile"
+echo "${IMG}" >> "$msgfile"
+echo "" >> "$msgfile"
+echo "## $(basename $(pwd))" >> "$msgfile"
+echo "Updates for $(basename $(pwd)) in the past day. $when" >> "$msgfile"
+echo "" >> "$msgfile"
+echo "" >> "$msgfile"
+git log --branches=\* --after="1 day ago" | sed 's/<.*@.*>/[email redacted]/g' | sed 's/^commit /#### commit /g' >> "$msgfile"
+echo "" >> "$msgfile"
+echo "" >> "$msgfile"
+sed -i 's/\&/\&amp;/g' "$msgfile"
+sed -i 's/</\&lt;/g' "$msgfile"
+sed -i 's/>/\&gt;/g' "$msgfile"
+sed -i 's/\t/    /g' "$msgfile"
+sed -i 's|  |\&nbsp; |g' "$msgfile"
+java -jar ~/git/SteemCliPoster/build/libs/SteemCliPoster.jar \
+	--auth-file ~/.steem/magali.properties \
+	--no-escape \
+	--file "$msgfile"
+cp tmp/log.new tmp/log.old
+
